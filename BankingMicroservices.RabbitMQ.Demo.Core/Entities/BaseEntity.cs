56598548aecb1e842e70f1ec8f2aa44296326997
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2024-10-22 00:04:22 +0200

    Refactor and change the structure of the folders in the solution

---
+++ BankingMicroservices.RabbitMQ.Demo.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingMicroservices.RabbitMQ.Demo.Core.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    [Timestamp] // Cancelation Token for concurency update
+    public byte[] RowVersion { get; set; } = [];
+}
