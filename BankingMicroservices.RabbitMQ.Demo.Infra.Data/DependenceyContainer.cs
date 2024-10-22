a718c10d16b232fceea93d8c81960104cb152f04
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2024-10-23 00:55:34 +0200

    Implement BankingMicroservices.RabbitMQ.Demo.Infra.Ioc project

---
+++ BankingMicroservices.RabbitMQ.Demo.Infra.Data/DependenceyContainer.cs
@@ -0,0 +1,14 @@
+﻿using BankingMicroservices.RabbitMQ.Demo.Core.Interfaces;
+using BankingMicroservices.RabbitMQ.Demo.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankingMicroservices.RabbitMQ.Demo.Infra.Ioc;
+
+public static class DependenceyContainer
+{
+    public static IServiceCollection RegisterEventBus(this IServiceCollection services)
+    {
+        services.AddSingleton<IEventBus, RabbitMqBus>();
+        return services;
+    }
+}
