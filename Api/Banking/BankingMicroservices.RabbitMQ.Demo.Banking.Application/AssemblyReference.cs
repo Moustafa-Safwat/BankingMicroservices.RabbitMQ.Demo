7f84f9eae564d741ee442dfe28e47b9dcc925dab
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2024-10-25 16:04:32 +0200

    Initialize the [BankingMicroservices.RabbitMQ.Demo.Banking.Application] project

---
+++ Api/Banking/BankingMicroservices.RabbitMQ.Demo.Banking.Application/AssemblyReference.cs
@@ -0,0 +1,17 @@
+﻿using BankingMicroservices.RabbitMQ.Demo.Core.Interfaces;
+using System.Reflection;
+
+namespace BankingMicroservices.RabbitMQ.Demo.Banking.Application
+{
+    /// <summary>
+    /// Provides a reference to the assembly containing the Banking Application.
+    /// Inherits from <see cref="BaseAssemblyReference"/>.
+    /// </summary>
+    public class AssemblyReference : BaseAssemblyReference
+    {
+        /// <summary>
+        /// Gets the assembly of the <see cref="AssemblyReference"/> type.
+        /// </summary>
+        public new readonly static Assembly Assembly = typeof(AssemblyReference).Assembly;
+    }
+}
--- Application/BankingMicroservices.RabbitMQ.Demo.Application/AssemblyReference.cs
+++ Application/BankingMicroservices.RabbitMQ.Demo.Application/AssemblyReference.cs
@@ -1,8 +1,16 @@
-﻿using System.Reflection;
+﻿using BankingMicroservices.RabbitMQ.Demo.Core.Interfaces;
+using System.Reflection;
 
 namespace BankingMicroservices.RabbitMQ.Demo.Application;
 
-public static class AssemblyReference
+/// <summary>
+/// Represents a reference to the assembly containing the current code.
+/// Inherits from <see cref="BaseAssemblyReference"/>.
+/// </summary>
+public class AssemblyReference : BaseAssemblyReference
 {
-    public readonly static Assembly Assembly = typeof(AssemblyReference).Assembly;
+    /// <summary>
+    /// Gets the assembly of the <see cref="AssemblyReference"/> type.
+    /// </summary>
+    public new readonly static Assembly Assembly = typeof(AssemblyReference).Assembly;
 }
