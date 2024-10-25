3c5326376bd8e62bab06ff947335174c26f1ed4a
Moustafa-Safwat <moustafa.safwat.iti.43@gmail.com>
2024-10-25 15:50:56 +0200

    Implement [BankingMicroservices.RabbitMQ.Demo.Banking.Infra.Ioc] project, and register repositories on it

---
+++ Api/Banking/BankingMicroservices.RabbitMQ.Demo.Banking.Infra.Ioc/DependenceyContainer.cs
@@ -0,0 +1,30 @@
+﻿using BankingMicroservices.RabbitMQ.Demo.Banking.Core.Entities;
+using BankingMicroservices.RabbitMQ.Demo.Banking.Core.Interfaces;
+using BankingMicroservices.RabbitMQ.Demo.Banking.Infra.Data.Repository;
+using BankingMicroservices.RabbitMQ.Demo.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankingMicroservices.RabbitMQ.Demo.Banking.Infra.Ioc;
+
+public static class DependenceyContainer
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers the repository services with the dependency injection container.
+    /// </summary>
+    /// <param name="services">The IServiceCollection to add the services to.</param>
+    /// <returns>The IServiceCollection with the registered services.</returns>
+    public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICrudRepository<Account>, CurdRepository<Account>>();
+        services.AddScoped<ICrudRepository<User>, CurdRepository<User>>();
+        services.AddScoped<IAccountRepository, AccountRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
+}
